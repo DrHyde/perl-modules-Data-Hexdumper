1f897b987f3b5c90079a11de6c5c656704d0ec5d
drhyde <drhyde>
2007-08-20 15:13:33 +0000

    Tidying, bugfixing

--- lib/Data/Hexdumper.pm
+++ lib/Data/Hexdumper.pm
@@ -1,10 +1,10 @@
 package Data::Hexdumper;
 
-$VERSION = "1.2";
+$VERSION = "1.3";
 
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw(hexdump Hexdump); # export Hexdump for bacwkard combatibility
+@EXPORT = qw(hexdump);
 
 use strict;
 use warnings;
@@ -14,52 +14,57 @@ use constant CHUNKSIZE => 16;
 
 # static data, tells us the length of each type of word
 my %num_bytes=(
-	C => 1, # unsigned char
-	S => 2, # unsigned short      (shorts are ALWAYS 16-bit)
-	n => 2, # big-endian short
-	v => 2, # little-endian short
-	L => 4, # unsigned long       (longs are ALWAYS 32-bit)
-	N => 4, # big-endian long
-	V => 4, # little-endian long
+    C => 1, # unsigned char
+    S => 2, # unsigned short      (shorts are ALWAYS 16-bit)
+    n => 2, # big-endian short
+    v => 2, # little-endian short
+    L => 4, # unsigned long       (longs are ALWAYS 32-bit)
+    N => 4, # big-endian long
+    V => 4, # little-endian long
 );
-
-=pod
 
 =head1 NAME
 
-Data::Hexdumper - A module for displaying binary data in a readable format
+Data::Hexdumper - Make binary data human-readable
 
 =head1 SYNOPSIS
 
-    use Data::Hexdumper;
+    use Data::Hexdumper qw(hexdump);
     $results = hexdump(
-        data => $data,          # what to dump
-        number_format => 'S',   # display as unsigned 'shorts'
-        start_position => 100,  # start at this offset ...
-        end_position => 148     # ... and end at this offset
+        data           => $data, # what to dump
+        number_format  => 'S',   # display as unsigned 'shorts'
+        start_position => 100,   # start at this offset ...
+        end_position   => 148    # ... and end at this offset
     );
     print $results;
 
 =head1 DESCRIPTION
 
-C<Data::Hexdumper> provides a simple way to format and display arbitary
-binary data in a way similar to how some debuggers do for lesser languages.
+C<Data::Hexdumper> provides a simple way to format arbitary binary data
+into a nice human-readable format, somewhat similar to the Unix 'hexdump'
+utility.
+
 It gives the programmer a considerable degree of flexibility in how the
 data is formatted, with sensible defaults.  It is envisaged that it will
 primarily be of use for those wrestling alligators in the swamp of binary
 file formats, which is why it was written in the first place.
 
-C<Data::Hexdumper> provides the following subroutines:
+=head1 SUBROUTINES
 
-=over 4
+The following subroutines are exported by default, although this is
+deprecated and will be removed in some future version.  Please pretend
+that you need to ask the module to export them to you.
 
-=item hexdump
+If you do assume that the module will always export them, then you may
+also assume that your code will break at some point after 1 Aug 2012.
 
-Does everything :-)  Takes a hash of parameters, one of which is mandatory,
+=head2 hexdump
+
+Does everything.  Takes a hash of parameters, one of which is mandatory,
 the rest having sensible defaults if not specified.  Available parameters
 are:
 
-=over 4
+=over
 
 =item data
 
@@ -84,10 +89,6 @@ and whether they are big- or little-endian.  The permissible values are
 C<C>, C<S>, C<n>, C<v>, C<L>, C<N>, and C<V>, having exactly the same
 meanings as they do in C<unpack>.  It defaults to 'C'.
 
-Note that 'short' and 'long' are always 16- and 32-bit respectively,
-regardless of what your C compiler thinks.  Syntax like 'S!' to get your
-compiler's notion of what a short might be is not supported at this time.
-
 =item suppress_warnings
 
 Make this true if you want to suppress any warnings - such as that your
@@ -102,98 +103,81 @@ spaces Otherwise, spaces will be printed as full stops / periods (ASCII
 
 =back
 
-=item Hexdump
+=head2 Hexdump - this function has now been removed
 
-This subroutine is deprecated and you are encouraged to use hexdump
-instead (note different capitalisation as that is more consistent with
-the perl idiom).  It is functionally identical to hexdump() with the
-exception that it will generate a warning when used unless you pass
-the suppress_warnings flag.
+The 'Hexdump' function (note the different capitalisation) was deprecated
+in version 1.0.1, and was removed in version 1.3 five years later.
 
 =cut
 
-sub VERSION {
-	return $Data::Hexdumper::VERSION;
-}
-
-sub Hexdump {
-	my %params = @_;
-	warn "Data::Hexdumper::Hexdump() is deprecated.\n".
-		     "please use Data::Hexdumper::hexdump() instead.\n".
-		     "note the lower-case h.\n"
-		unless($params{suppress_warnings});
-	return hexdump(@_); } # for backwards combatibility
-
 sub hexdump {
-	my %params=@_;
-	my($data, $number_format, $start_position, $end_position)=
-		@params{qw(data number_format start_position end_position)};
+    my %params=@_;
+    my($data, $number_format, $start_position, $end_position)=
+        @params{qw(data number_format start_position end_position)};
 
-	# sanity-check the parameters
+    my $addr = $start_position ||= 0;
+    $number_format ||= 'C';
+    $end_position ||= length($data)-1;
+    my $num_bytes = $num_bytes{$number_format};
 
-	die("No data given to hexdump.") unless $data;
+    # sanity-check the parameters
 
-	my $addr = $start_position ||= 0;
-	die("start_position must be numeric.") if($start_position=~/\D/);
+    die("No data given to hexdump.") unless $data;
+    die("start_position must be numeric.") if($start_position=~/\D/);
+    die("number_format $number_format not recognised.") unless $num_bytes;
+    die("end_position must be numeric.") if($end_position=~/\D/);
+    die("end_position must not be before start_position.")
+        if($end_position < $start_position);
 
-	$number_format ||= 'C';
-	my $num_bytes=$num_bytes{$number_format};
-	die("number_format $number_format not recognised.") unless $num_bytes;
+    # extract the required range and pad end with NULLs if necessary
 
-	$end_position ||= length($data)-1;
-	die("end_position must be numeric.") if($end_position=~/\D/);
-	die("end_position must be after start position.")
-		if($end_position <= $start_position);
+    $data=substr($data, $start_position, 1+$end_position-$start_position);
+    if(length($data)/$num_bytes != int(length($data)/$num_bytes)) {
+        warn "Data::Hexdumper: data doesn't exactly fit into an integer number ".
+                 "of '$number_format' words,\nso has been padded ".
+                 "with NULLs at the end.\n"
+            unless($params{suppress_warnings});
+        $data .= pack('C', 0) x ($num_bytes - length($data) + int(length($data)/$num_bytes)*$num_bytes);
+    }
 
-	# extract the required range and pad end with NULLs if necessary
+    my $output=''; # where we put the formatted results
 
-	$data=substr($data, $start_position, 1+$end_position-$start_position);
-	if(length($data)/$num_bytes != int(length($data)/$num_bytes)) {
-		warn "data doesn't exactly fit into an integer number ".
-			     "of '$number_format' words, so has been\npadded ".
-			     "with NULLs at the end.\n"
-			unless($params{suppress_warnings});
-		$data .= pack('C', 0) x ($num_bytes - length($data) + int(length($data)/$num_bytes)*$num_bytes);
-	}
+    while($data) {
+        # Get a chunk
+        my $chunk = substr($data, 0, CHUNKSIZE);
+        $data = ($chunk eq $data) ? '' : substr($data, CHUNKSIZE);
+        
+        $output.=sprintf('  0x%04X : ', $addr);
 
-	my $output=''; # where we put the formatted results
+        # have to keep chunk for printing, so make a copy we
+        # can 'eat' $num_bytes at a time.
+        my $line=$chunk;
 
-	while($data) {
-		# Get a chunk
-		my $chunk = substr($data, 0, CHUNKSIZE);
-		$data = ($chunk eq $data) ? '' : substr($data, CHUNKSIZE);
-		
-		$output.=sprintf('  0x%04X : ', $addr);
+        my $lengthOfLine=0;         # used for formatting in inner loop
 
-		# have to keep chunk for printing, so make a copy we
-		# can 'eat' $num_bytes at a time.
-		my $line=$chunk;
-
-		my $lengthOfLine=0;         # used for formatting in inner loop
-
-		while(length($line)) {
-			# grab a $num_bytes element, and remove from line
-			my $thisElement=substr($line,0,$num_bytes);
-			if(length($line)>$num_bytes) {
-				$line=substr($line,$num_bytes);
-			} else { $line=''; }
-			my $thisData=sprintf('%0'.($num_bytes*2).'X ',
-				       unpack($number_format, $thisElement));
-			$lengthOfLine+=length($thisData);
-			$output.=$thisData;
-		}
-		# replace any non-printable character with .
-		if($params{space_as_space}) {
-		    $chunk=~s/[^a-z0-9\\|,.<>;:'\@[{\]}#`!"\$%^&*()_+=~?\/ -]/./gi;
+        while(length($line)) {
+            # grab a $num_bytes element, and remove from line
+            my $thisElement=substr($line,0,$num_bytes);
+            if(length($line)>$num_bytes) {
+                $line=substr($line,$num_bytes);
+            } else { $line=''; }
+            my $thisData=sprintf('%0'.($num_bytes*2).'X ',
+                       unpack($number_format, $thisElement));
+            $lengthOfLine+=length($thisData);
+            $output.=$thisData;
+        }
+        # replace any non-printable character with .
+        if($params{space_as_space}) {
+            $chunk=~s/[^a-z0-9\\|,.<>;:'\@[{\]}#`!"\$%^&*()_+=~?\/ -]/./gi;
                 }
                 else {
-		    $chunk=~s/[^a-z0-9\\|,.<>;:'\@[{\]}#`!"\$%^&*()_+=~?\/-]/./gi;
+            $chunk=~s/[^a-z0-9\\|,.<>;:'\@[{\]}#`!"\$%^&*()_+=~?\/-]/./gi;
                 }
-		# Yes, this 48 *is* a magic number.
-		$output.=' ' x (48-$lengthOfLine) .": $chunk\n";
-		$addr += CHUNKSIZE;
-	}
-	$output;
+        # Yes, this 48 *is* a magic number.
+        $output.=' ' x (48-$lengthOfLine) .": $chunk\n";
+        $addr += CHUNKSIZE;
+    }
+    $output;
 }
 
 =head1 SEE ALSO
@@ -206,39 +190,40 @@ perldoc -f unpack
 
 perldoc -f pack
 
-=head1 BUGS
+=head1 BUGS/LIMITATIONS
 
 There is no support for syntax like 'S!' like what pack() has, so it's
-not possible to tell it to use your environment's native word-lengths
-are, only 16- and 32-bit shorts and longs are supported.
+not possible to tell it to use your environment's native word-lengths.
+Only 16- and 32-bit shorts and longs are supported.  There is no support
+for 64-bit datatypes.
 
 It formats the data for an 80 column screen, perhaps this should be a
 frobbable parameter.
 
 Formatting may break if the end position has an address greater than 65535.
 
-=head1 AUTHOR
+=head1 FEEDBACK
 
-David Cantrell (david@cantrell.org.uk).
+I welcome constructive criticism and bug reports.  Please report bugs either
+by email or via RT:
+  L<http://rt.cpan.org/Public/Dist/Display.html?Name=Data-Hexdumper>
 
-=head1 HISTORY
+The best bug reports contain a test file that fails with the code that is
+currently in CVS, and will pass once it has been fixed.  The CVS repository
+is on Sourceforge and can be viewed in a web browser here:
+  L<http://drhyde.cvs.sourceforge.net/drhyde/perlmodules/Data-Hexdumper/>
 
-=item Version 0.01 
+=head1 AUTHOR, COPYRIGHT and LICENCE
 
-Original version.
+This software is copyright 2001 - 2007 David Cantrell (david@cantrell.org.uk).
 
-=item Version 1.01
+You may use, modify and distribute this software under the same terms as
+you may perl itself.
 
-The lack of bug reports convinced me that 0.01 was ready for release, so I
-bumped it up to 1.00 (which was never released) then remembered to fix the
-documented bug where you tried to dump data whose length wasn't an integer
-multiple of your word length.
+=head1 THANKS TO ...
 
-=item Version 1.1
-
-Fixed bug where it emitted extra warnings in some places.
+MHX, for reporting a bug when dumping a single byte of data
 
 =cut
 
 1;
-
--- t/all.t
+++ t/all.t
@@ -1,16 +1,11 @@
-use strict; use warnings;
-my $loaded;
+#!perl -w
+use strict;
 
-BEGIN { $| = 1; print "1..9\n"; }
-END { print "not ok 1\n" unless $loaded; }
+use Test::More tests => 10;
 
-use Data::Hexdumper;
-$loaded = 1;
-my $test = 1;
-print "ok $test\n";
+use_ok('Data::Hexdumper', 'hexdump');
 
-# trivial test, big-endian 32-bit words, no padding
-print "not " unless("\n".hexdump(
+ok("\n".hexdump(
 	data => join('', map { pack('C', $_) } (0x20 .. 0x3F)),
 	number_format => 'N',
 	start_position => 0,
@@ -18,10 +13,9 @@ print "not " unless("\n".hexdump(
 ) eq q{
   0x0000 : 20212223 24252627 28292A2B 2C2D2E2F             : .!"#$%&'()*+,-./
   0x0010 : 30313233 34353637 38393A3B 3C3D3E3F             : 0123456789:;<=>?
-});
-print 'ok '.++$test."\n";
+}, "big-endian 32-bit words, no padding");
 
-print "not " unless("\n".hexdump(
+ok("\n".hexdump(
 	data => join('', map { pack('C', $_) } (0x20 .. 0x3F)),
 	number_format => 'N',
 	start_position => 0,
@@ -30,28 +24,24 @@ print "not " unless("\n".hexdump(
 ) eq q{
   0x0000 : 20212223 24252627 28292A2B 2C2D2E2F             :  !"#$%&'()*+,-./
   0x0010 : 30313233 34353637 38393A3B 3C3D3E3F             : 0123456789:;<=>?
-});
-print 'ok '.++$test."\n";
+}, "space_as_space");
 
-# same as above, make sure it figgers out start_position and end_position
-print "not " unless("\n".hexdump(
+ok("\n".hexdump(
 	data => join('', map { pack('C', $_) } (0x20 .. 0x3F)),
 	number_format => 'N'
 ) eq q{
   0x0000 : 20212223 24252627 28292A2B 2C2D2E2F             : .!"#$%&'()*+,-./
   0x0010 : 30313233 34353637 38393A3B 3C3D3E3F             : 0123456789:;<=>?
-});
-print 'ok '.++$test."\n";
+}, "start_position and end_position defaults");
 
 my $results = '';
 foreach my $format(qw (C n v V)) { # same trivial test for other formats
-    # $results .= "\n" if($results);
     $results .= hexdump(
         data => join('', map { pack('C', $_) } (0x20 .. 0x3F)),
         number_format => $format 
     );
 }
-print "not " unless("\n".$results eq q{
+ok("\n".$results eq q{
   0x0000 : 20 21 22 23 24 25 26 27 28 29 2A 2B 2C 2D 2E 2F : .!"#$%&'()*+,-./
   0x0010 : 30 31 32 33 34 35 36 37 38 39 3A 3B 3C 3D 3E 3F : 0123456789:;<=>?
   0x0000 : 2021 2223 2425 2627 2829 2A2B 2C2D 2E2F         : .!"#$%&'()*+,-./
@@ -60,21 +50,17 @@ print "not " unless("\n".$results eq q{
   0x0010 : 3130 3332 3534 3736 3938 3B3A 3D3C 3F3E         : 0123456789:;<=>?
   0x0000 : 23222120 27262524 2B2A2928 2F2E2D2C             : .!"#$%&'()*+,-./
   0x0010 : 33323130 37363534 3B3A3938 3F3E3D3C             : 0123456789:;<=>?
-});
-print 'ok '.++$test."\n";
+}, "other data formats");
 
-# now see if unprintable characters are rendered properly
-print "not " unless("\n".hexdump(
+ok("\n".hexdump(
 	data => join('', map { pack('C', $_) } (0x10 .. 0x2F)),
 	number_format => 'N'
 ) eq q{
   0x0000 : 10111213 14151617 18191A1B 1C1D1E1F             : ................
   0x0010 : 20212223 24252627 28292A2B 2C2D2E2F             : .!"#$%&'()*+,-./
-});
-print 'ok '.++$test."\n";
+}, "unprintable characters");
 
-# now check that S eq n or v and L eq N or V
-print "not" unless(hexdump(
+ok(hexdump(
 	data => join('', map { pack('C', $_) } (0x10 .. 0x2F)),
 	number_format => 'S'
 ) eq hexdump(
@@ -86,9 +72,9 @@ print "not" unless(hexdump(
 ) eq hexdump(
 	data => join('', map { pack('C', $_) } (0x10 .. 0x2F)),
 	number_format => 'v'
-));
-print 'ok '.++$test."\n";
-print "not" unless(hexdump(
+), "S eq n or v");
+
+ok(hexdump(
 	data => join('', map { pack('C', $_) } (0x10 .. 0x2F)),
 	number_format => 'L'
 ) eq hexdump(
@@ -100,10 +86,8 @@ print "not" unless(hexdump(
 ) eq hexdump(
 	data => join('', map { pack('C', $_) } (0x10 .. 0x2F)),
 	number_format => 'V'
-));
-print 'ok '.++$test."\n";
+), "L eq N or V");
 
-# FIXME - now see what happens when we pad data
 $results = '';
 foreach my $format (qw(N n)) {
 	foreach my $max (0x3C, 0x3D, 0x3E) {
@@ -114,8 +98,7 @@ foreach my $format (qw(N n)) {
     	);
 	}
 }
-# print "\n$results";
-print "not " unless("\n".$results eq q{
+ok("\n".$results eq q{
   0x0000 : 20212223 24252627 28292A2B 2C2D2E2F             : .!"#$%&'()*+,-./
   0x0010 : 30313233 34353637 38393A3B 3C000000             : 0123456789:;<...
   0x0000 : 20212223 24252627 28292A2B 2C2D2E2F             : .!"#$%&'()*+,-./
@@ -128,5 +111,9 @@ print "not " unless("\n".$results eq q{
   0x0010 : 3031 3233 3435 3637 3839 3A3B 3C3D              : 0123456789:;<=
   0x0000 : 2021 2223 2425 2627 2829 2A2B 2C2D 2E2F         : .!"#$%&'()*+,-./
   0x0010 : 3031 3233 3435 3637 3839 3A3B 3C3D 3E00         : 0123456789:;<=>.
-});
-print 'ok '.++$test."\n";
+}, "NULL-padding");
+
+$results = hexdump(data => '!');
+ok("\n".$results eq q{
+  0x0000 : 21                                              : !
+}, "Single byte data");
